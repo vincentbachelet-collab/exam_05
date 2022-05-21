fc2feb7b60cc319c247533e7c7e7a1f34f0aae7c
vincentbachelet-collab <vcbachelet@gmail.com>
2022-05-21 16:43:03 +0200

    ex00 finished

--- ex_00/Warlock.cpp
+++ ex_00/Warlock.cpp
@@ -6,30 +6,33 @@
 /*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 11:33:08 by vbachele          #+#    #+#             */
-/*   Updated: 2022/05/13 12:00:45 by vbachele         ###   ########.fr       */
+/*   Updated: 2022/05/21 16:42:01 by vbachele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Warlock.hpp"
 
-// CANONICAL FORM //
-
 Warlock::Warlock(std::string name, std::string title) : _name(name), _title(title)
 {
-	std::cout<< name <<": This looks like another boring day." << std::endl;
+	std::cout << name << ": This looks like another boring day" << std::endl;
 }
 
-Warlock::Warlock(const Warlock &rhs)
-{
-	this->_name = rhs->_name;
-	this->_title = rhs->title;
-	*this = rhs;
-	std::cout<< this->_name <<": This looks like another boring day." << std::endl;
+Warlock::~Warlock (void) {
+	std::cout << this->_name << ": My job here is done!" << std::endl;
 }
 
-Warlock Warlock::&operator=(const Warlock &obj)
-~Warlock::Warlock(void)
-{
-	std::cout<< this->_name << ": My job here is done!" << std::endl;
+std::string		Warlock::getName(void) const {
+	return (this->_name);
 }
 
+std::string		Warlock::getTitle(void) const {
+	return (this->_title);
+}
+
+void			Warlock::setTitle(std::string const &new_title) {
+	this->_title = new_title;
+}
+
+void 			Warlock::introduce() const {
+	std::cout << this->_name << ": I am " << this->_name << " " << this->_title << "!" << std::endl;
+}
--- ex_00/Warlock.hpp
+++ ex_00/Warlock.hpp
@@ -6,30 +6,31 @@
 /*   By: vbachele <vbachele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 11:33:09 by vbachele          #+#    #+#             */
-/*   Updated: 2022/05/13 11:56:03 by vbachele         ###   ########.fr       */
+/*   Updated: 2022/05/21 16:36:28 by vbachele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include WARLOCK_HPP
-# define WARLOCK_HPP
-#include <iostream>
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+#include "iostream"
 
-class Warlock
-{
-	public:
-	//Canonical functions//
-	Warlock(std::string name, std::string title);
-	Warlock &operator=(const Warlock &obj);
-	Warlock(const Warlock &obj);
+
+class Warlock {
+public:
+	// Canonical form //
+	Warlock (std::string const name, std::string const title);
+	//Warlock (Warlock const &old_obj);
+	//Warlock &operator=(Warlock const &rhs);
 	~Warlock(void);
 
-	// Members functions //
+	// members functions //
+	// const at the end to avoid to change name/title
 	std::string	getName(void) const;
-	void		setTitle(std::string &title) const;
 	std::string getTitle(void) const;
-	void		introduce() const;
+	void		setTitle(std::string const &new_title);
+	void		introduce(void) const;
 
-	private:
+private:
 	std::string _name;
 	std::string _title;
 };
